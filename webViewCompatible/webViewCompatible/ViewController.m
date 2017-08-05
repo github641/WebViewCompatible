52ad4e0e379981636e89cee4708cf8c4d67a73ce
刘志意 <liuzhiyi@alldk.com>
2017-08-05 14:24:18 +0800

    看下SafariViewController的delegate方法返回activity数组的作用

--- webViewCompatible/webViewCompatible/ViewController.m
+++ webViewCompatible/webViewCompatible/ViewController.m
@@ -14,6 +14,8 @@
 #import "TOWebViewController.h"
 #import "DZNWebViewController.h"
 
+#import "TOActivitySafari.h"
+
 #import "Macro.h"
 
 @interface ViewController ()<SFSafariViewControllerDelegate>
@@ -39,8 +41,43 @@
     
     // 判断，当前显示的控制器是否有 navi管理：有则push，无则present
     
-    [WebviewCompatibleTool from:self toWebBrowserWithPush:NO url:s];
+//    [WebviewCompatibleTool from:self toWebBrowserWithPush:NO url:s];
     
+   SFSafariViewController *vc = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:s]];
+    [self presentViewController:vc animated:YES completion:nil];
+    
+    vc.delegate = self;
+}
+
+
+#pragma mark - ================== SFSafariViewControllerDelegate ==================
+
+/*! @abstract Called when the view controller is about to show UIActivityViewController after the user taps the action button.
+ @param URL the URL of the web page.
+ @param title the title of the web page.
+ @result Returns an array of UIActivity instances that will be appended to UIActivityViewController.
+ */
+- (NSArray<UIActivity *> *)safariViewController:(SFSafariViewController *)controller activityItemsForURL:(NSURL *)URL title:(nullable NSString *)title{
+    NSLog(@"%s url:%@ title:%@", __func__, URL, title);
+    
+    return  @[[TOActivitySafari new]];
+
+}
+
+/*! @abstract Delegate callback called when the user taps the Done button. Upon this call, the view controller is dismissed modally. */
+- (void)safariViewControllerDidFinish:(SFSafariViewController *)controller{
+    NSLog(@"%s", __func__);
+}
+
+
+/*! @abstract Invoked when the initial URL load is complete.
+ @param success YES if loading completed successfully, NO if loading failed.
+ @discussion This method is invoked when SFSafariViewController completes the loading of the URL that you pass
+ to its initializer. It is not invoked for any subsequent page loads in the same SFSafariViewController instance.
+ */
+- (void)safariViewController:(SFSafariViewController *)controller didCompleteInitialLoad:(BOOL)didLoadSuccessfully{
+    NSLog(@"%s didLoadSuccessfully:%@", __func__, @(didLoadSuccessfully));
+
 }
 
 
--- webViewCompatible/webViewCompatible/WebviewCompatibleTool.m
+++ webViewCompatible/webViewCompatible/WebviewCompatibleTool.m
@@ -85,33 +85,4 @@
     
 }
 
-//#pragma mark - ================== SFSafariViewControllerDelegate ==================
-
-/*! @abstract Called when the view controller is about to show UIActivityViewController after the user taps the action button.
- @param URL the URL of the web page.
- @param title the title of the web page.
- @result Returns an array of UIActivity instances that will be appended to UIActivityViewController.
- */
-//- (NSArray<UIActivity *> *)safariViewController:(SFSafariViewController *)controller activityItemsForURL:(NSURL *)URL title:(nullable NSString *)title{
-//    NSLog(@"%s", __func__);
-//    return @[];
-//}
-//
-///*! @abstract Delegate callback called when the user taps the Done button. Upon this call, the view controller is dismissed modally. */
-//- (void)safariViewControllerDidFinish:(SFSafariViewController *)controller{
-//    NSLog(@"%s", __func__);
-//}
-//
-//
-///*! @abstract Invoked when the initial URL load is complete.
-// @param success YES if loading completed successfully, NO if loading failed.
-// @discussion This method is invoked when SFSafariViewController completes the loading of the URL that you pass
-// to its initializer. It is not invoked for any subsequent page loads in the same SFSafariViewController instance.
-// */
-//- (void)safariViewController:(SFSafariViewController *)controller didCompleteInitialLoad:(BOOL)didLoadSuccessfully{
-//    NSLog(@"%s didLoadSuccessfully:%@", __func__, @(didLoadSuccessfully));
-//
-//}
-
-
 @end
